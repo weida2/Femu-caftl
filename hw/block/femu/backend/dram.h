9cef308f238c812fd3dc517b85ed62f1a42c6035
Wei Xiangj <63967300+weida2@users.noreply.github.com>
2023-02-23 18:02:40 +0800

    Initial commit

---
+++ hw/block/femu/backend/dram.h
@@ -0,0 +1,29 @@
+#ifndef __FEMU_MEM_BACKEND
+#define __FEMU_MEM_BACKEND
+
+/**
+ * @file dram.h
+ * @brief DRAM后端相关声明
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <stdint.h>
+
+/**
+ * @brief  DRAM backend SSD address space
+ * 
+ */
+typedef struct SsdDramBackend {
+    void    *logical_space;
+    int64_t size; /**< in bytes */
+    int     femu_mode;
+} SsdDramBackend;
+
+int init_dram_backend(SsdDramBackend **mbe, int64_t nbytes);
+void free_dram_backend(SsdDramBackend *);
+
+int backend_rw(SsdDramBackend *, QEMUSGList *, uint64_t *, bool);
+
+#endif
